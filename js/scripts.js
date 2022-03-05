25faf043dacac6f18dd1df1cdc67366d56342280
Daniela T <94473375+Peneloppe-jack@users.noreply.github.com>
2022-03-05 16:00:25 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+// The idea is simple jQuery is supposed to simplify code
+// Creating an app with jQuery 
+// a to do list allowing you to 
+// add and delete items, change orders of items
+
+function newItem(){
+// Adding a new item to the list of items: 
+//see mini tutorial : Getting input values
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    $('#list').append(li);
+  }
+
+function crossOut() {
+  li.classList.toggle("strike");
+}
+//Crossing out item with Event handling mini tuto 
+	li.on("dblclick", function crossOut() {
+		li.toggleClass("strike");
+	});
+
+
+//Adding a delete button X appending HTML button 
+//deleteButton.append(document.createTextNode('X')); 
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+  crossOutButton.on("click", deleteListItem);
+
+  function deleteListItem(){
+		li.addClass("delete");
+  	}
+
+// reordering items ref to list of functions available with JQ
+   $('#list').sortable();
+  }
+
+
